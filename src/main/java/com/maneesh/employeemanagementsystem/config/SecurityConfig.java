b54de9885a6b6f68a1bbe637a65596791b9f9de2
medasm20 <medasaimaneesh2004@gmail.com>
2026-07-08 23:58:03 +0530

    Title Changed to Employee Management System

--- src/main/java/com/maneesh/employeemanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/maneesh/employeemanagementsystem/config/SecurityConfig.java
@@ -56,3 +56,4 @@ public class SecurityConfig {
         return http.build();
     }
 }
+
--- src/main/java/com/maneesh/employeemanagementsystem/model/Role.java
+++ src/main/java/com/maneesh/employeemanagementsystem/model/Role.java
@@ -17,4 +17,4 @@ public class Role {
 
     @NotBlank(message = "Role Name is required")
     private String roleName;
-}
+} 
