1bf0c1cb01b05aac292977a5fdf275450bfac1c2
medasm20 <medasaimaneesh2004@gmail.com>
2026-07-16 15:31:44 +0530

    Removed unnecessary exception declaration

--- src/main/java/com/maneesh/employeemanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/maneesh/employeemanagementsystem/config/SecurityConfig.java
@@ -18,8 +18,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
 
         http
                 .cors(Customizer.withDefaults())
